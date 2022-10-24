92dadc83c7a2281ca56b0c1c5679fd25ede1773c
Akib Hasan <ahratul740@gmail.com>
2022-10-24 18:50:29 +0200

    update

--- pages/5_📫 Get In Touch With Me!.py
+++ pages/5_📫 Get In Touch With Me!.py
@@ -8,7 +8,7 @@ with hc.HyLoader('LOADING', hc.Loaders.standard_loaders, index=[2, 2, 2, 2]):
     time.sleep(2)
 
 contact_form = """
-<form action="https://formsubmit.co/YOUREMAIL@EMAIL.COM" method="POST">
+<form action="https://formsubmit.co/ahratul740@gmail.com" method="POST">
      <input type="hidden" name="_captcha" value="false">
      <input type="text" name="name" placeholder="Your name" required>
      <input type="email" name="email" placeholder="Your email" required>
@@ -18,7 +18,22 @@ contact_form = """
 """
 
 st.markdown(contact_form, unsafe_allow_html=True)
+import streamlit as st
+import base64
+import requests
 
+def render_svg(svg):
+    """Renders the given svg string."""
+    b64 = base64.b64encode(svg.encode('utf-8')).decode("utf-8")
+    html = r'<img src="data:image/svg+xml;base64,%s"/>' % b64
+    st.write(html, unsafe_allow_html=True)
+
+
+url = "https://raw.githubusercontent.com/ahratul/ahratul/output/github-contribution-grid-snake.svg"
+r = requests.get(url) # Get the webpage
+svg = r.content.decode() # Decoded response content with the svg string
+
+render_svg(svg)
 # Use Local CSS File
 def local_css(file_name):
     with open(file_name) as f:
