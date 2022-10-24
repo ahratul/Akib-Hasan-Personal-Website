35a2135d5502442605ab26d45e56fdb46a42ba88
Akib Hasan <ahratul740@gmail.com>
2022-10-24 17:59:05 +0200

    update

--- pages/5_📫 Get In Touch With Me!.py
+++ pages/5_📫 Get In Touch With Me!.py
@@ -3,7 +3,7 @@ import loaders as hc
 import time
 
 st.header(":mailbox: Get In Touch With Me!")
-st.subheader("📫Get In Touch With Me!📫")
+st.subheader("Get In Touch With Me!")
 st.write("---")
 with hc.HyLoader('LOADING', hc.Loaders.standard_loaders, index=[2, 2, 2, 2]):
     time.sleep(2)
